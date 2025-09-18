d432042a5f4b3d3437abe6520007f83b7c64c979
NoWayLoL06 <yongjun6617@gmail.com>
2025-09-18 23:18:19 +0900

    main

---
+++ src/Foo.java
@@ -0,0 +1,13 @@
+public class Foo {
+    public static void main(String[] args){
+        byte b = 127;
+        int i = 100;
+
+        System.out.println(b + i);
+        System.out.println(10/4);
+        System.out.println(10.0/4);
+        System.out.println((char)0x12340061);
+        System.out.println((int)2.0 + (int)1.8); // 2 + 1
+        System.out.println((int)2.9 + 1.8);
+
+}
