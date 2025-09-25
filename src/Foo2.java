090067142b5377ea3ae40074407f3f0ef72a8590
NoWayLoL06 <yongjun6617@gmail.com>
2025-09-25 15:27:41 +0900

    main

---
+++ src/Foo2.java
@@ -0,0 +1,14 @@
+public class Foo2 {
+    public static void main(String[] args){
+        int a = 33, b = 13;
+        if(a%2 == 0){
+            System.out.println("a is even");
+            System.out.println("congraturation!!");
+        }
+        System.out.println("a is odd");
+    }
+
+
+        
+
+}
